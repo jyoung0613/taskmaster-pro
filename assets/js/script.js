8edf3972034896377961de5e45bc5bc4d15cc3aa
John Young <jwyoung0613@gmail.com>
2021-08-23 07:33:45 -0600

    added jQuery UI code to make a sortable list and an temp array to store data

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,11 +34,15 @@ var loadTasks = function() {
   // loop over object properties
   $.each(tasks, function(list, arr) {
     console.log(list, arr);
-    // then loop over sub-array
+    //then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
+      
     });
-  });
+
+  })
+
+
 };
 
 var saveTasks = function() {
@@ -70,7 +74,7 @@ $("#task-form-modal .btn-primary").click(function() {
     $("#task-form-modal").modal("hide");
 
     // save in tasks array
-    tasks.toDo.push({
+    tasks.toDo.push ({
       text: taskText,
       date: taskDate
     });
@@ -163,6 +167,60 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+});
+
+// add jQuery UI code to create a sortable list out of the cards
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      // trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+      // add task data to the temp array as an object 
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    console.log(tempArr);
+  }
 });
 
 // remove all tasks
